da0ff7aba50b48920b53cf1e20ab2a67838941c5
Brendan Rielly <brendan.rielly@endurance.com>
2018-11-27 17:00:54 -0500

    GraphQL yoga implementation with resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const Mutations = {
+   async createItem(parent, args, context, info) {
+       // TODO: Check if they are logged in
 
-module.exports = mutations;
+       const item = await context.db.mutation.createItem({
+           data: {
+               ...args
+           }
+       }, info);
+
+       return item;
+   }
+};
+
+module.exports = Mutations;
